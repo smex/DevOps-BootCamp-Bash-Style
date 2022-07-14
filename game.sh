deddb2e76e18728d383a9b2fa226b8f6daeee543
smex <cnexxx@gmail.com>
2022-07-15 01:56:00 +0300

    first release

--- game.sh
+++ game.sh
@@ -1,28 +1,30 @@
+#!/bin/bash
+
 echo -e "\n NumberJack \n"
 ch=0
 while [ $ch -ne 3 ]; do
-    echo  "  
+    echo "  
          PLAY : Hit 1 and enter.
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-    read -p "Enter your choice : " ch
-    if [ $ch -eq 1 ];then
+    read -rp "Enter your choice : " ch
+    if [ "$ch" -eq 1 ];then
     x=0 ;c=0 ;p=0
-    read -p "Enter any number between 0 and 9 : " n
+    read -rp "Enter any number between 0 and 9 : " n
     while [ $c -eq 0 ];
     do
-        x=11; r=($(shuf -i 0-9 -n 10))
-        echo "${r[@]} "; for i in {1..10}; do
+        x=11; r=("$(shuf -i 0-9 -n 10)")
+        echo "${r[@]}"; for i in {1..10}; do
             a[$i]=$i
         done
-        echo "${a[@]} "
-        read -t 5 -p "Enter the index of your number : " x
+        echo "${a[@]}"
+        read -rt 5 -p "Enter the index of your number : " x
     if [[ $? -gt 128 ]]; then
     c=1
     break
     fi
-if [ ${r[$(($x))-1]} -eq $n ];then
+if [ "${r[$(("$x"))-1]}" -eq "$n" ];then
             echo "Great"
     ((p=p+1))
         else
@@ -30,7 +32,7 @@ if [ ${r[$(($x))-1]} -eq $n ];then
             break
         fi
     done
-    elif [ $ch -eq 2 ];then
+    elif [ "$ch" -eq 2 ];then
         echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
 else
         break
